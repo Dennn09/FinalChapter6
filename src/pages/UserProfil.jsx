6bb1bf7757673a55b485e35539c122094af55ac6
21.11.4377 Deni Masulili <97225549+Dennn09@users.noreply.github.com>
2023-11-04 04:33:13 +0700

    push

--- src/pages/UserProfil.jsx
+++ src/pages/UserProfil.jsx
@@ -3,22 +3,26 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export const UserProfil = () => {
-  const dataUser = useSelector(
-    (state) => state.reduxGetUser.dataUser.data.data
-  );
+  const dataUser = useSelector((state) => state?.reduxGetUser?.dataUser?.data?.data);
 
   const navigate= useNavigate()
 
-  console.log(dataUser);
-
+  console.log(dataUser, "dataUser");
   return (
     <div className="bg-gradient-to-bl from-red-500 via-yellow-900 to-black w-screen h-screen flex justify-center items-center">
-    <div className="border-2 w-2/5 h-3/10 backdrop-blur shadow-lg text-white font-semibold p-4">
-      <h1 className="text-2xl mb-2">Nama: {dataUser.name}</h1>
-      <h1>Email: {dataUser.email}</h1>
-      <button className="bg-red-500 p-[5px]" onClick={()=>{navigate("/dashboard")}}>Back dashboard</button>
+      <div className="border-2 w-2/5 h-3/10 backdrop-blur shadow-lg text-white font-semibold p-4">
+        {dataUser ? (
+          <>
+            <h1 className="text-2xl mb-2">Nama: {dataUser?.name}</h1>
+            <h1>Email: {dataUser?.email}</h1>
+          </>
+        ) : (
+          <p>Data pengguna tidak tersedia.</p>
+        )}
+        <button className="bg-red-500 p-[5px]" onClick={() => navigate("/dashboard")}>
+          Back to dashboard
+        </button>
+      </div>
     </div>
-  </div>
-  
   );
 };
